5f5ed5747402e258ec6151a295b133d4cacceffc
Anish Zala <azala@affirma.com>
2023-01-24 16:23:13 +0530

    Add project files.

---
+++ ProductWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebAPI.Services;
+
+namespace ProductWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService productService;
+
+        public ProductsController()
+        {
+            productService = new ProductService();
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(productService.GetProducts());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(string id)
+        {
+            return Ok(productService.GetProduct(id));
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            int rowsAffected = productService.AddProduct(product);
+            if (rowsAffected > 0)
+                return Ok("Added");
+            else return Ok("Error occured");
+        }
+    }
+}
